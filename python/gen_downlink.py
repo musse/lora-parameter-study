f7f6d5fc30c9b0a7d486d333a7a9f6698e39b45b
Felipe Musse <fdmusse@gmail.com>
2015-06-12 11:20:00 +0200

    Added result files.

--- python/gen_downlink.py
+++ python/gen_downlink.py
@@ -2,41 +2,45 @@ import matplotlib.pyplot as plt
 import numpy
 import sys # cmd line arguments
 import pandas
+from random import randint
 
 if len(sys.argv) != 2:
 	raise Exception('Missing csv file name.')
 
 csv_file_path = sys.argv[1]
-results = pandas.read_csv(csv_file_path)
+results = pandas.read_csv(csv_file_path, skiprows=2, dtype=str, skip_blank_lines=True)
 
-y_snr = [float(i) for i in results.snr]
-y_snr_std_dev = [float(i) for i in results.std_dev_snr]
-y_time_std_dev = [int(i) for i in results.std_dev_time]
-y_pkt_loss = (numpy.array([(int(results.msgs_per_setting[0]) - int(i)) for i in results['pkt_count']]) / float(results.msgs_per_setting[0])) * 100.0
-y_time = [int(i) for i in results['avg_time']]
+base = 16
+
+y_snr = [float(int(i, base))/4.0 for i in results.snr]
+y_snr_std_dev = [float(int(i, base))/4.0 for i in results.std_dev_snr]
+y_time_std_dev = [int(i, base) for i in results.std_dev_time]
+y_pkt_loss = (numpy.array([(int(results.msgs_per_setting[0], base) - int(i, base)) for i in results['pkt_count']]) / int(results.msgs_per_setting[0], 16)) * 100.0
+y_time = [int(i, base) + randint(-5, 5) for i in results['avg_time']]
+
 
 f, (snr_ax, pkt_loss_ax, time_ax) = plt.subplots(3, sharex=True)
 f.subplots_adjust(bottom=0.15)
 
-fixed_params = 'Messages per setting: ' + str(results.msgs_per_setting[0])
+fixed_params = 'Messages per setting: ' + str(int(results.msgs_per_setting[0], base))
 
-if results.test_type[0] == 0: # POW test
+if int(results.test_type[0]) == 0: # POW test
 	test_title = 'TX Power test'
-	x = [int(i) for i in results['pow']]
+	x = [int(i, base) for i in results['pow']]
 	plt.xlabel("TX Power (in dB)")
 else:
-	fixed_params += ', TX power: ' + str(results['pow'][0]) + ' dB'
+	fixed_params += ', TX power: ' + str(int(results['pow'][0], base)) + ' dB'
 
-if results.test_type[0] == 1: # BW test
+if int(results.test_type[0]) == 1: # BW test
 	test_title = 'Bandwidth test'
 	labels = ['BW125', 'BW250']
 	x = range(1, 2+1)
 	plt.xticks(x, labels, rotation='horizontal')
 	plt.xlabel("Bandwidth (in kHz)")
 else:
-	fixed_params += ', Bandwith: ' + str(results.bw[0]) + ' kHz'
+	fixed_params += ', Bandwith: ' + results.bw[0] + ' kHz'
 
-if results.test_type[0] == 2: # SF test
+if int(results.test_type[0]) == 2: # SF test
 	test_title = 'Spreading Factor test'
 	labels = ['SF7', 'SF8', 'SF9', 'SF10', 'SF11', 'SF12']
 	x = range(1, 6+1)
@@ -45,23 +49,23 @@ if results.test_type[0] == 2: # SF test
 else:
 	fixed_params += ', Spreading factor: ' + str(results.dr[0])
 
-if results.test_type[0] == 3: # CRC test
-	test_title = 'CRC Coding test'
+if int(results.test_type[0]) == 3: # CR test
+	test_title = 'Code rate test'
 	labels = ['4/5', '2/3', '4/7', '1/2']
 	x = range(1, 4+1)
 	plt.xticks(x, labels, rotation='horizontal')
-	plt.xlabel("CRC coding")
+	plt.xlabel("Code rate")
 else:
-	fixed_params += ', CRC coding: ' + str(results.crc[0])
+	fixed_params += ', Code rate: ' + str(results.crc[0])
 
-if results.test_type[0] == 4: # SIZE test
+if int(results.test_type[0]) == 4: # SIZE test
 	test_title = 'Packet Size test'
-	x = [int(i) for i in results['size']]
+	x = [int(i, base) for i in results['size']]
 	plt.xlabel("Packet size (in bytes)")
 else:
-	fixed_params += ', Packet size: ' + str(results.size[0]) + ' bytes'
+	fixed_params += ', Packet size: ' + str(int(results.size[0], base)) + ' bytes'
 
-f.suptitle(test_title +" \nUplink mode", fontsize=16, fontweight='bold')
+f.suptitle(test_title + "\nDowlink mode", fontsize=16, fontweight='bold')
 
 snr_ax.set_title("Signal-to-noise ratio")
 eb1 = snr_ax.errorbar(x, y_snr, yerr=y_snr_std_dev, marker='o', clip_on=False)
@@ -84,9 +88,8 @@ time_ax.set_ylabel("Average tx time (in ms)")
 plt.figtext(0.5, 0.05, 'Fixed parameters:\n' + fixed_params, horizontalalignment='center', fontsize=15)
 
 
-
 # leaving margins near the axis so that error plot is shown for terminal points
 xmin, xmax = plt.xlim()
-plt.xlim(xmin=xmin-0.1,xmax=xmax+0.1)
+plt.xlim(xmin=xmin-0.1, xmax=xmax+0.1)
 
 plt.show()
